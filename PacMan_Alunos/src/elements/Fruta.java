04300ca26fe5db3626ae4da610a52c721da5e1f5
Gabriel-S-R <gsrib.121098@gmail.com>
2017-12-13 15:05:10 -0200

    Erasing non-necessary code lines

--- PacMan_Alunos/src/elements/Fruta.java
+++ PacMan_Alunos/src/elements/Fruta.java
@@ -5,32 +5,18 @@ import java.awt.Graphics;
 import java.io.Serializable;
 import java.util.Timer;
 import java.util.TimerTask;
-import utils.Consts;
 
 public class Fruta extends Element implements Serializable{
-    private int contIntervalos;
     Timer timer;
     
     public Fruta(String imageName) {
         super(imageName);
         this.isMortal = false;
-        this.contIntervalos = 0;
-        //setTimerFruta();
     }
     
     @Override
     public void autoDraw(Graphics g){
         Drawing.draw(g, this.imageIcon, pos.getY(), pos.getX());
-    
-    
-   /* this.contIntervalos++;
-        if(this.contIntervalos == Consts.TIMER_FRUTA){
-            this.contIntervalos = 0;
-            Fruta f = new Fruta("fruta.png");
-            f.setPosition(5,5);
-            Drawing.getGameScreen().addElement(f);
-            setTimer();
-        }*/
     }
   
     public void setTimerFruta() {
